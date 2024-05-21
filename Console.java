c379116b374f34a9b51a2bd272459943a2287361
emaridette <embmarmol@gmail.com>
2024-05-21 13:48:21 -0400

    whoops just got javac

--- Console.java
+++ Console.java
@@ -3,7 +3,6 @@ import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-
 public class Console {
 	private static Scanner in;
 	//λ
--- Function.java
+++ Function.java
@@ -1,3 +1,3 @@
 public class Function implements Expression {
-
+    
 }
