992f94a2736411698dadace8f7beec248e1334c0
anaajesh <anaghaajesh2010@gmail.com>
2024-06-10 23:16:31 -0400

    edit

--- Runner.java
+++ Runner.java
@@ -2,59 +2,58 @@
  * Anagha Ajesh & Ella Marmol
  */
 
-import java.util.HashSet;
+ import java.util.HashSet;
 
-public class Runner {
-    public static final HashSet<String> freeVarNames = new HashSet<>();
+ public class Runner {
+     public static final HashSet<String> freeVarNames = new HashSet<>();
+ 
+     private static Expression runHelper(Expression exp) {
+         if (exp instanceof Application app) {
+             if (app.left instanceof Function func) {
+                 Variable var = func.variable;
+                 Expression funcExp = func.expression;
+                 Expression subExp = app.right.copy();
+                 if (subExp instanceof Variable freeVar) {
+                     freeVarNames.add(freeVar.name);
+                 }
+                 return funcExp.sub(var, subExp);
+             } else {
+                 Expression temp = runHelper(app.left);
+                 if (temp != null) {
+                     app.left = temp;
+                     return app;
+                 }
+                 temp = runHelper(app.right);
+                 if (temp != null) {
+                     app.right = temp;
+                     return app;
+                 }
+             }
+         } else if (exp instanceof Function func) {
+             Expression temp = runHelper(func.expression);
+             if (temp != null) {
+                 func.expression = temp;
+                 return func;
+             }
+         }
+         return null;
+     }
+ 
+     public static Expression run(Expression exp) {
+         freeVarNames.clear();
+         Expression subExp = runHelper(exp);
+         while (subExp != null) {
+             exp = subExp;
+             subExp = runHelper(exp);
+         }
 
-    private static Expression runHelper(Expression exp) {
-        if (exp instanceof Application app) {
-            if (app.left instanceof Function func) {
-                Variable var = func.variable;
-                Expression funcExp = func.expression;
-                Expression subExp = app.right.copy();
-                if (subExp instanceof Variable freeVar) {
-                    freeVarNames.add(freeVar.name);
-                }
-                return funcExp.sub(var, subExp);
-            } else {
-                Expression temp = runHelper(app.left);
-                if (temp != null) {
-                    app.left = temp;
-                    return app;
-                }
-                temp = runHelper(app.right);
-                if (temp != null) {
-                    app.right = temp;
-                    return app;
-                }
-            }
-        } else if (exp instanceof Function func) {
-            Expression temp = runHelper(func.expression);
-            if (temp != null) {
-                func.expression = temp;
-                return func;
-            }
-        }
-        return null;
-    }
-
-    public static Expression run(Expression exp) {
-        freeVarNames.clear();
-        Expression subExp = runHelper(exp);
-        while (subExp != null) {
-            exp = subExp;
-            subExp = runHelper(exp);
-        }
-        // compare this expression against everything in stored variables. If this
-        // evaluates to an expression that is identical to a stored variable, just
-        // return that variable
-        for (String key : Parser.storedVariables.keySet()) {
-
-            if (exp.equals(Parser.storedVariables.get(key))) {
-                return new Variable(key);
-            }
-        }
-        return exp;
-    }
-}
+         for (String key : Parser.storedVariables.keySet()) {
+ 
+             if (exp.equals(Parser.storedVariables.get(key))) {
+                 return new Variable(key);
+             }
+         }
+         return exp;
+     }
+ }
+ 
