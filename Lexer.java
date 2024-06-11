442bba75d383d7e256871021c95dba4a7d6f0ec6
anaajesh <anaghaajesh2010@gmail.com>
2024-06-10 23:36:07 -0400

    lexer

--- Lexer.java
+++ Lexer.java
@@ -2,55 +2,56 @@
  * Anagha Ajesh & Ella Marmol
  */
 
-import java.util.ArrayList;
-import java.util.Arrays;
-
-public class Lexer {
-    static final ArrayList<String> characters = new ArrayList<>(Arrays.asList("\\", ".", "(", ")", "λ", "="));
-    static final String spaceString = " ";
-
-    /*
-     * A lexer (or "tokenizer") converts an input into tokens that
-     * eventually need to be interpreted.
-     *
-     * Given the input
-     * (\bat .bat flies)cat λg.joy! )
-     * you should output the ArrayList of strings
-     * [(, \, bat, ., bat, flies, ), cat, \, g, ., joy!, )]
-     *
-     */
-    public ArrayList<String> tokenize(String input) {
-
-        //tokenlist
-        ArrayList<String> tokens = new ArrayList<>();
-
-        //traverse the input
-        for (int i = 0; i < input.length() && input.charAt(i) != ';';) {
-            String currentChar = input.substring(i, i + 1);
-
-            
-            if (currentChar.equals(";")) {
-                break;
-            } else if (currentChar.equals(spaceString)) {
-                //skip spaces
-                ++i;
-            } else if (characters.contains(currentChar)) {
-                //check for special characters
-                if (currentChar.equals("λ")) {
-                    tokens.add("\\");
-                } else {
-                   tokens.add(currentChar);
-                }
-                ++i;
-            } else {
-                int start = i;
-                while (!(i == input.length() || input.charAt(i) == ' '
-                        || characters.contains(input.substring(i, i + 1)))) {
-                    ++i;
-                }
-                tokens.add(input.substring(start, i));
-            }
-        }
-        return tokens;
-    }
-}
+ import java.util.ArrayList;
+ import java.util.Arrays;
+ 
+ public class Lexer {
+     static final ArrayList<String> characters = new ArrayList<>(Arrays.asList("\\", ".", "(", ")", "λ", "="));
+     static final String spaceString = " ";
+ 
+     /*
+      * A lexer (or "tokenizer") converts an input into tokens that
+      * eventually need to be interpreted.
+      *
+      * Given the input
+      * (\bat .bat flies)cat λg.joy! )
+      * you should output the ArrayList of strings
+      * [(, \, bat, ., bat, flies, ), cat, \, g, ., joy!, )]
+      *
+      */
+     public ArrayList<String> tokenize(String input) {
+ 
+         //tokenlist
+         ArrayList<String> tokens = new ArrayList<>();
+ 
+         //traverse the input
+         for (int i = 0; i < input.length() && input.charAt(i) != ';';) {
+             String currentChar = input.substring(i, i + 1);
+ 
+             
+             if (currentChar.equals(";")) {
+                 break;
+             } else if (currentChar.equals(spaceString)) {
+                 //skip spaces
+                 ++i;
+             } else if (characters.contains(currentChar)) {
+                 //check for special characters
+                 if (currentChar.equals("λ")) {
+                     tokens.add("\\");
+                 } else {
+                    tokens.add(currentChar);
+                 }
+                 ++i;
+             } else {
+                 int start = i;
+                 while (!(i == input.length() || input.charAt(i) == ' '
+                         || characters.contains(input.substring(i, i + 1)))) {
+                     ++i;
+                 }
+                 tokens.add(input.substring(start, i));
+             }
+         }
+         return tokens;
+     }
+ }
+ 
